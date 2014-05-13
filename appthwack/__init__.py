5ed8230858aa9ed79a09a6f07035e4ee46b0679b
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2014-05-13 17:25:05 +0200

    Catch ImportError as it would be raised by setup.py when requests are not yet installed

--- appthwack/__init__.py
+++ appthwack/__init__.py
@@ -8,4 +8,9 @@ __name__ = 'appthwack'
 __author__ = 'Andrew Hawker <andrew@appthwack.com>'
 __version__ = '1.0.0'
 
-from appthwack import AppThwackApi, AppThwackApiError
+try:
+    from appthwack import AppThwackApi, AppThwackApiError
+except ImportError:
+    # this can occur before package is installed as requets dependency is not
+    # yet resolved -- setup.py could never install the package because of this
+    pass
